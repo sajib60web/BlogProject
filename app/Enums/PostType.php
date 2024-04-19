717bd71a07b65613686f73a5a9b2ccce14438b7e
sajib60web <sajib60web@gmail.com>
2024-04-19 23:30:05 +0600

    mage

--- app/Enums/PostType.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\Enums;
-interface PostType{
-    const ARTICLE = 1;
-    const VIDEO   = 2;
-}
--- app/Enums/Status.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Enums;
-interface Status{
-    const ACTIVE     = 1;
-    const INACTIVE   = 2;
-    const PUBLISH    = 3;
-    const UNPUBLISH  = 4;
-}
