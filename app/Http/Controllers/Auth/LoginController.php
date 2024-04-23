505d6651f9646769072e0dd958f27ef20538d33d
sajib60web <sajib60web@gmail.com>
2024-04-24 00:13:22 +0600

    Update authenticated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\BlockStatus;
+use App\Enums\Status;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,25 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->status == Status::INACTIVE) {
+            Auth::guard('web')->logout();
+            $notification = array(
+                'message' => 'Your account status is InActive',
+                'alert-type' => 'error'
+            );
+            return redirect('/login')->with($notification);
+        }
+        if ($user->block_status == BlockStatus::BLOCK) {
+            Auth::guard('web')->logout();
+            $notification = array(
+                'message' => 'Your account status is block',
+                'alert-type' => 'error'
+            );
+            return redirect('/login')->with($notification);
+        }
     }
 }
